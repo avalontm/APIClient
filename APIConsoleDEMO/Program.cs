30f70d534280eb19ae309c15724c47986f6c6abe
Λv∂lΘи ™ <jaimeraulmendezlopez325@hotmail.com>
2022-10-08 14:25:42 -0700

    updated

---
+++ APIConsoleDEMO/Program.cs
@@ -0,0 +1,11 @@
+﻿using APIClient;
+
+
+Console.WriteLine(Client.client.GetAsync(""));
+
+
+public static class Client
+{
+    static WebClientManager _client;
+    public static WebClientManager client => _client ?? (_client = new WebClientManager(string.Format("{0}/api", "")));
+}
